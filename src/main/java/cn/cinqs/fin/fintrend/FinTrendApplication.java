4546fcd766f307c3d2da0f3299755db6a5226f36
Ci SONG <ci.song@cisong.eu>
2017-10-09 21:54:23 +0800

    inited project

---
+++ src/main/java/cn/cinqs/fin/fintrend/FinTrendApplication.java
@@ -0,0 +1,12 @@
+package cn.cinqs.fin.fintrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTrendApplication.class, args);
+	}
+}
